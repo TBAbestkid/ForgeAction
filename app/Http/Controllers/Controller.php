4e09bc7533ade37cc675b945f4a19cc4c5031946
Kharen Aguiar <kharenaguiar@gmail.com>
2025-06-13 14:05:34 -0300

    Controller apneas
    tá retornando as view, não tem nada mais.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    // Aqui vai ser o controller de personagem sim.
+    public function index(){
+        return view('personagem.index');
+    }
+    // Exibe a view de registro
+    public function about(){
+        return view('about');
+    }
+
 }
