485557b96aaa06ea83a2e993ce229a32ded4e350
Kharen Aguiar <kharenaguiar@gmail.com>
2025-06-13 13:18:52 -0300

    Criação de ApiService
    Futuramente ajusto melhor o registerUser, mas o foco é o login, onde então eu uso o basic auth que é algo necessario, com o adm roger, e realizo o seguinte post 'login' e 'senha', retornaod um json

---
+++ app/services/ExternalApiService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class ExternalApiService
+{
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'https://total-alice-rokaideveloper-77325d04.koyeb.app'; 
+    }
+
+    // Método GET
+    public function registerUser($login, $senha)
+    {
+        $response = Http::post("{$this->baseUrl}/chave_personagem", [
+            'chapLogin' => $login,
+            'chapSenha' => $senha,
+        ]);
+
+        return $response->json();
+    }
+
+    public function getChapId($login)
+    {
+        $response = Http::get("{$this->baseUrl}/chave_personagem/check/{$login}");
+        return $response->json();
+    }
+
+    public function loginUser($login, $senha)
+    {
+        $response = Http::withBasicAuth('roger', 'roger@123')
+            ->post("{$this->baseUrl}/login", [
+                'login' => $login,
+                'senha' => $senha,
+            ]);
+
+        return $response->json();
+    }
+
+    public function getPersonagem($token)
+    {
+        $response = Http::withToken($token)
+            ->get("{$this->baseUrl}/info_personagem/{$this->getChapId()}"); // ou outro endpoint
+
+        if ($response->ok()) {
+            return $response->json();
+        }
+
+        return null;
+    }
+
+    // Supondo que você tenha o chapId guardado
+    // protected function getChapId()
+    // {
+    //     return session('chap_id'); // ou de onde você está salvando o chap_id
+    // }
+
+}
