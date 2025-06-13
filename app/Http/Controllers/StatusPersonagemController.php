3c3898233e0a09fb877fa6048cf87b87ff8bcfd7
Kharen Aguiar <kharenaguiar@gmail.com>
2025-06-13 18:43:38 -0300

    Adicionando apenas paginas, que provavelmente serão ajustado

--- app/Http/Controllers/StatusPersonagemController.php
+++ app/Http/Controllers/StatusPersonagemController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class StatusPersonagemController extends Controller
 {
-    //
+    // pagina principal
+    public function exibicao()
+    {
+        return view('personagem.index');
+    }
+
+    // Pagina de criar personagem
+    public function criar(){
+        return view('personagem.criar');
+    }
+
+    // Pagina de atualizar personagem 
+    public function atualiza(){
+        return view('personagem.edit');
+    }
+    
+    // Pagina de deletar personagem
+    public function deleta(){
+        // bem, não tem segredo, deletar né?
+    }
+    
+    
 }
