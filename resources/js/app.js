92671e506e85329e3711908f65f6ead28e1819c3
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-08-21 14:53:06 -0300

    Inserindo variaveis globais

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { apiRequest, showModal, showToast } from './api.js';
+
+// Exemplo de uso global
+window.apiRequest = apiRequest;
+window.showModal = showModal;
+window.showToast = showToast;
