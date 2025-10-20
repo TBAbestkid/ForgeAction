a3780eb1733f6b2158835be6ff93ed07c9461279
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-10-20 16:04:46 -0300

    Utilizando agora a chave da API brave para enviar um email, além de claro, tornamos o brevo como transporte principal sem utilizar smtp

---
+++ app/Mail/Transport/BrevoTransport.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail\Transport;
+
+use Illuminate\Mail\Transport\Transport;
+use Swift_Mime_SimpleMessage;
+use Illuminate\Support\Facades\Http;
+
+class BrevoTransport extends Transport
+{
+    protected $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
+    {
+        $this->beforeSendPerformed($message);
+
+        $to = [];
+        foreach ($message->getTo() as $email => $name) {
+            $to[] = ['email' => $email, 'name' => $name];
+        }
+
+        $response = Http::withHeaders([
+            'api-key' => $this->apiKey,
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ])->post('https://api.brevo.com/v3/smtp/email', [
+            'sender' => [
+                'name' => $message->getFrom()[key($message->getFrom())],
+                'email' => key($message->getFrom()),
+            ],
+            'to' => $to,
+            'subject' => $message->getSubject(),
+            'htmlContent' => $message->getBody(),
+        ]);
+
+        return $response->successful() ? 1 : 0;
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Transport\BrevoTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Mail::extend('brevo', function($app) {
+            return new \Illuminate\Mail\Mailer(
+                $app['view'],
+                $app['swift.mailer']->getTransport() // Não usado, mas obrigatório
+            )->setSwiftMailer(new \Swift_Mailer(new BrevoTransport(env('MAIL_BREVO_API_KEY'))));
+        });
+
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
