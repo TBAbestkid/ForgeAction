a7d553618818f1cdb0516b1f0dd55ba271e66ad4
Kharen Aguiar <kharenaguiar@gmail.com>
2025-10-21 19:35:16 -0300

    Adicionando para implemetnar futuramente o login com google

--- config/services.php
+++ config/services.php
@@ -39,5 +39,10 @@ return [
         'user' => env('EXTERNAL_API_USER', 'roger'),
         'pass' => env('EXTERNAL_API_PASS', 'roger@123'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 
 ];
