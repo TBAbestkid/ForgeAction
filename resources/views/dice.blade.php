9b9d149eaeaae521f32f9a710985146f129a142c
Kharen Aguiar <kharenaguiar@gmail.com>
2025-11-12 20:02:00 -0300

    Update dice.blade.php

--- resources/views/dice.blade.php
+++ resources/views/dice.blade.php
@@ -3,19 +3,32 @@
 @section('title', 'Dados Teste - Controle Total')
 
 @section('content')
+
 <div id="scene-container" style="width: 400px; height: 400px;"></div>
 
 <script type="module">
-    import DiceBox from 'https://unpkg.com/@3d-dice/dice-box-threejs/dist/dice-box-threejs.es.js';
+    // import DiceBox from 'https://unpkg.com/@3d-dice/dice-box-threejs/dist/dice-box-threejs.es.js';
+    import DiceBox from '@3d-dice/dice-box-threejs';
 
-    const box = new DiceBox("#scene-container", {
+    const Box = new DiceBox("#scene-container", {
         assetPath: 'https://unpkg.com/@3d-dice/dice-box-threejs/dist/',
         scale: 5,
         theme: 'default',
+        onRollComplete: (result) => {
+            console.log('Roll result:', result);
+        }
     });
 
-    await box.init();
-    await box.roll('1d20@18');
-    console.log('Roll complete');
+    Box.initalize()
+        .then(() => {
+            setTimeout(() => {
+                Box.roll('1d20@18');
+            }, 1000);
+        })
+        .catch((error) => {
+            console.error('Error initializing DiceBox:', error);
+        });
+
+    console.log('DiceBox initialized');
 </script>
 @endsection
