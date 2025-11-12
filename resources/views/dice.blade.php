ad7b135522555f844b954f122287e1e40f300777
Kharen Aguiar <kharenaguiar@gmail.com>
2025-11-12 20:26:01 -0300

    Update dice.blade.php

--- resources/views/dice.blade.php
+++ resources/views/dice.blade.php
@@ -2,12 +2,13 @@
 @section('title', 'Simulador de Rolagem de Dados')
 @section('content')
 <div class="container my-4">
-  <h1>Simulador de Rolagem</h1>
+    <h1>Simulador de Rolagem</h1>
 
-  <input id="dice-notation" class="form-control" placeholder="1d20" />
-  <button id="roll-btn" class="btn btn-primary">Rolar</button>
-  <div id="scene-container" style="width: 400px; height: 400px;"></div>
+    <input id="dice-notation" class="form-control" placeholder="1d20" />
+    <input type="number" id="forceValue" class="form-control mt-2" placeholder="Forçar valor"/>
+    <button id="roll-btn" class="btn btn-primary">Rolar</button>
+    <div id="scene-container" style="width: 400px; height: 400px;"></div>
 </div>
 
-@vite(['resources/js/dice.js'])
+@vite(['resources/js/dados-teste.js'])
 @endsection
