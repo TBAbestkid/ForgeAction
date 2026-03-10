6786497a5764742039e4a0a9aff2354bc5ddca3f
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2026-03-09 21:45:32 -0300

    🎨 style(entercode): Ajusta cores de texto e fundo do modal `Entrar na sala com código`

--- resources/views/partials/entercode.blade.php
+++ resources/views/partials/entercode.blade.php
@@ -1,16 +1,16 @@
 <!-- Modal: Inserir Código -->
 <div class="modal fade" id="modalSalabyCode" tabindex="-1" aria-labelledby="modalCodeLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content text-dark">
-            <div class="modal-header bg-light">
-                <h1 class="modal-title fs-5 d-flex align-items-center" id="modalCodeLabel">
-                    <i class="fa-solid fa-key text-primary me-2"></i> Entrar na sala com código
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5 d-flex align-items-center text-white" id="modalCodeLabel">
+                    <i class="fa-solid fa-key text-white me-2"></i> Entrar na sala com código
                 </h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
             </div>
             <div class="modal-body">
-                <p class="text-secondary mb-3">Digite o código da sala que você recebeu:</p>
-                <input type="text" id="inputCodigoSala" class="form-control form-control-lg text-center" placeholder="Ex: ABC123" maxlength="10">
+                <p class="text-secondary mb-3 text-white">Digite o código da sala que você recebeu:</p>
+                <input type="text" id="inputCodigoSala" class="form-control form-control-lg text-center text-white" placeholder="Ex: ABC123" maxlength="10">
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
