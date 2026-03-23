59a9545ef243c258f63c40b4e7005513b534981b
TBAbestkid <kharenaguiar@gmail.com>
2026-03-23 20:28:00 -0300

    🐛 fix(sala): Garante imagem de fundo padrão

--- resources/views/room/room.blade.php
+++ resources/views/room/room.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 <div id="roomBackground"
-     style="background-image: url('{{ $sala['urlBackground'] }}')">
+     style="background-image: url('{{ $sala['urlBackground'] ? $sala['urlBackground'] : '/assets/images/forge.png' }}');">
 </div>
 
 <div class="position-absolute top-0 end-0 d-flex align-items-center gap-2 m-3">
