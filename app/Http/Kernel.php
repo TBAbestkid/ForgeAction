f553077c27ba7bd8b58125bbd1a5a4666f1ba81b
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2026-06-12 16:10:29 -0300

    ✨ feat(kernel): Adiciona middleware UserSession
    Adiciona o middleware `EnsureUserSession` ao arquivo `app/Http/Kernel.php` para garantir que a sessão do usuário esteja ativa.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'user.session' => \App\Http\Middleware\EnsureUserSession::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
